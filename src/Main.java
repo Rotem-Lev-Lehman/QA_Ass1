cb6ff55613b86165b22667b3080c91f5463f07d1
Rotem-Lev-Lehman <Rotem12430@gmail.com>
2019-11-27 22:06:22 +0200

    Added the bonus function and reused identical code

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,52 @@
 public class Main {
 
     public static void main(String[] args) {
-        NamesMaster namesMaster=new NamesMaster();
-        try{
-            if (args.length ==1){
-                if(args[0].equals("GenerateName")){
-
-                }
-                else {
+        NamesMaster namesMaster = new NamesMaster();
+        try {
+            if (args.length == 1) {
+                if (args[0].equals("GenerateName")) {
+                    namesMaster.GenerateName();
+                } else {
                     badInput();
                 }
-            }
-            else if (args.length ==2){
-                if(args[0].equals("CountSpecificString")){
+            } else if (args.length == 2) {
+                if (args[0].equals("CountSpecificString")) {
                     namesMaster.CountSpecificString(args[1]);
-                }
-                else if(args[0].equals("CountAllStrings")){
+                } else if (args[0].equals("CountAllStrings")) {
                     int length = Integer.parseInt(args[1]);
-                    if(length<=0){
+                    if (length <= 0) {
                         badInput();
-                    }
-                    else {
+                    } else {
                         namesMaster.CountAllStrings(length);
                     }
-                }
-                else if(args[0].equals("CountMaxString")){
+                } else if (args[0].equals("CountMaxString")) {
                     int length = Integer.parseInt(args[1]);
-                    if(length<=0){
+                    if (length <= 0) {
                         badInput();
-                    }
-                    else {
+                    } else {
                         namesMaster.CountMaxString(length);
                     }
-                }
-                else if(args[0].equals("AllIncludesString")){
+                } else if (args[0].equals("AllIncludesString")) {
                     namesMaster.AllIncludesString(args[1]);
-                }
-                else {
+                } else {
                     badInput();
                 }
-            }
-            else {
+            } else {
                 badInput();
             }
-        }
-        catch (Exception e){
+            /*
+            //namesMaster.CountSpecificString("As");
+            //namesMaster.CountAllStrings(9);
+            //namesMaster.CountMaxString(5);
+            namesMaster.GenerateName();
+            */
+        } catch (Exception e) {
             badInput();
         }
         namesMaster.finished();
     }
 
-    private static void badInput(){
+    private static void badInput() {
         System.out.println("Bad parameters");
     }
 }
--- src/NamesMaster.java
+++ src/NamesMaster.java
@@ -1,4 +1,3 @@
-
 import java.io.*;
 import java.net.URL;
 import java.util.*;
@@ -15,77 +14,142 @@ public class NamesMaster {
         startReading();
     }
 
-    public void CountSpecificString(String name){
+    public void CountSpecificString(String name) {
         int counter = 0;
         String nextName;
-        while((nextName=nextName())!=null){
-            if(nextName.contains(name)){
+        while ((nextName = nextName()) != null) {
+            if (nextName.contains(name)) {
                 counter++;
             }
         }
         System.out.println(counter);
     }
 
-    public void CountAllStrings(int length){
-        Map<String,Integer> dictionary = getDictByLength(length,false);
-        for (Map.Entry<String,Integer> pair:dictionary.entrySet()) {
-            System.out.println(pair.getKey()+":"+pair.getValue());
+    public void CountAllStrings(int length) {
+        Map<String, Integer> dictionary = getDictByLength(length, false);
+        for (Map.Entry<String, Integer> pair : dictionary.entrySet()) {
+            System.out.println(pair.getKey() + ":" + pair.getValue());
         }
     }
 
-    private Map<String, Integer> getDictByLength(int length,boolean toLower) {
-        Map<String,Integer> dictionary = new HashMap<String,Integer>();
+    private Map<String, Integer> getDictByLength(int length, boolean toLower) {
+        Map<String, Integer> dictionary = new HashMap<>();
         String nextName;
-        while((nextName=nextName())!=null){
+        while ((nextName = nextName()) != null) {
             if (toLower) {
                 nextName = nextName.toLowerCase();
             }
-            for(int i=0;i+length<nextName.length();i++){
-                String str = nextName.substring(i,i+length);//check if add 1
-                int quantity = 0;
-                if(dictionary.containsKey(str)){
-                    quantity = dictionary.get(str);
-                }
-                dictionary.put(str,quantity+1);
+            for (int i = 0; i + length < nextName.length(); i++) {
+                String str = nextName.substring(i, i + length);//check if add 1
+                addOneToQuantity(dictionary, str);
             }
         }
         return dictionary;
     }
 
-    public void CountMaxString(int length){
-        Map<String,Integer> dictionary = getDictByLength(length,true);
-        List<String> maxStrings = new LinkedList<>();
-        int maxQuantity = -1;
-        for (Map.Entry<String,Integer> pair:dictionary.entrySet()) {
-            if (pair.getValue() >= maxQuantity){
-                if (pair.getValue() > maxQuantity){
-                    maxQuantity = pair.getValue();
-                    maxStrings = new LinkedList<>();
-                }
-                maxStrings.add(pair.getKey());
-            }
-        }
-        for (String str:maxStrings) {
+    public void CountMaxString(int length) {
+        Map<String, Integer> dictionary = getDictByLength(length, true);
+        List<String> maxStrings = getArgMaxList(dictionary);
+        for (String str : maxStrings) {
             System.out.println(str);
         }
     }
 
-    public void AllIncludesString(String name){
+    public void AllIncludesString(String name) {
         String nextName;
-        while((nextName=nextName())!=null){
-            if(name.contains(nextName)){
+        while ((nextName = nextName()) != null) {
+            if (name.contains(nextName)) {
                 System.out.println(nextName);
             }
         }
     }
 
-    public void GenerateName(){
+    public void GenerateName() {
+        //Initialize data structures for generating the name:
+        Map<Character, Integer> firstLettersAppearances = new HashMap<>();
+        Map<Character, Map<Character, Integer>> pairsCounting = new HashMap<>();
+        InitializeDictionaries(firstLettersAppearances, pairsCounting);
 
+        //Generate the name:
+        String generatedName = GenerateName(firstLettersAppearances, pairsCounting);
+
+        //Print the name:
+        System.out.println(generatedName);
     }
 
+    private String GenerateName(Map<Character, Integer> firstLettersAppearances, Map<Character, Map<Character, Integer>> pairsCounting) {
+        String name = "";
+        Character prevLetter = createFirstLetter(firstLettersAppearances);
+        name += prevLetter;
+        for (int i = 0; i < 5; i++) {
+            prevLetter = createNextLetter(pairsCounting, prevLetter);
+            name += prevLetter;
+        }
+        return name;
+    }
 
+    private void InitializeDictionaries(Map<Character, Integer> firstLettersAppearances, Map<Character, Map<Character, Integer>> pairsCounting) {
+        String nextName;
+        while ((nextName = nextName()) != null) {
+            //Handle first characters dictionary:
+            char first = nextName.charAt(0);
+            addOneToQuantity(firstLettersAppearances, first);
 
-    private void startReading(){
+            //Handle pairs dictionary:
+            for (int i = 1; i < nextName.length(); i++) {
+                char curr = nextName.charAt(i);
+                char prev = nextName.charAt(i - 1);
+                Map<Character, Integer> prevDict;
+                if (pairsCounting.containsKey(prev)) {
+                    prevDict = pairsCounting.get(prev);
+                } else {
+                    prevDict = new HashMap<>();
+                    pairsCounting.put(prev, prevDict);
+                }
+                addOneToQuantity(prevDict, curr);
+            }
+        }
+    }
+
+    private Character createFirstLetter(Map<Character, Integer> firstLettersAppearances) {
+        return getRandomArgMax(firstLettersAppearances);
+    }
+
+    private Character createNextLetter(Map<Character, Map<Character, Integer>> pairsCounting, Character prevLetter) {
+        Map<Character, Integer> prevDict = pairsCounting.get(prevLetter);
+        return getRandomArgMax(prevDict);
+    }
+
+    private static <T> T getRandomArgMax(Map<T, Integer> dictionary) {
+        List<T> argMax = getArgMaxList(dictionary);
+        int r = (int) (Math.random() * argMax.size());
+        return argMax.get(r);
+    }
+
+    private static <T> List<T> getArgMaxList(Map<T, Integer> dictionary) {
+        int maxAmount = -1;
+        List<T> maxList = new ArrayList<>();
+        for (Map.Entry<T, Integer> entry : dictionary.entrySet()) {
+            if (entry.getValue() >= maxAmount) {
+                if (entry.getValue() > maxAmount) {
+                    maxAmount = entry.getValue();
+                    maxList = new ArrayList<>();
+                }
+                maxList.add(entry.getKey());
+            }
+        }
+        return maxList;
+    }
+
+    private static <T> void addOneToQuantity(Map<T, Integer> dictionary, T key) {
+        int quantity = 0;
+        if (dictionary.containsKey(key)) {
+            quantity = dictionary.get(key);
+        }
+        dictionary.put(key, quantity + 1);
+    }
+
+    private void startReading() {
         try {
             FileReader fileReader = new FileReader(file);
             bufferedReader = new BufferedReader(fileReader);
@@ -94,7 +158,7 @@ public class NamesMaster {
         }
     }
 
-    public void finished(){
+    public void finished() {
         try {
             bufferedReader.close();
         } catch (IOException e) {
@@ -102,11 +166,11 @@ public class NamesMaster {
         }
     }
 
-    private String nextName(){
+    private String nextName() {
         String name = null;
         try {
             name = bufferedReader.readLine();
-            if (name!=null){
+            if (name != null) {
                 name = formatName(name);
             }
         } catch (IOException e) {
@@ -116,7 +180,7 @@ public class NamesMaster {
     }
 
     private String formatName(String name) {
-        name = name.substring(0,1).toUpperCase()+name.substring(1).toLowerCase();
+        name = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
         return name;
     }
 }
